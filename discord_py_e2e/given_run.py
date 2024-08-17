0c77fe771a7344cb5b0ee0cfe2309748ff14b6d5
Blue <blue@nqn.blue>
2024-08-17 16:12:21 +0100

    Make 'I run command' take quotes

--- discord_py_e2e/given_run.py
+++ discord_py_e2e/given_run.py
@@ -4,7 +4,7 @@ from behave.api.async_step import async_run_until_complete
 from discord_py_e2e.context import Context
 
 
-@given("I run {command}")
+@given("I run '{command}'")
 @async_run_until_complete
 async def step_run_command(context: Context, command: str):
     context.command_message = await context.channel.send(command)
