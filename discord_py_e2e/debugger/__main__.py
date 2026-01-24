97a36d92f65db9bea5ea6e79b825b74b20e13e13
Blue <blue@nqn.blue>
2026-01-24 11:05:50 +0000

    Improve the debugger CLI

--- discord_py_e2e/debugger/__main__.py
+++ discord_py_e2e/debugger/__main__.py
@@ -1,19 +1,38 @@
 import asyncio
+import sys
 
-from discord import TeamMember
 from discord_py_e2e.debugger.connection import connect_to_nqn
 
 
-def _get_bot_owner() -> TeamMember:
+def _eval(code: str) -> str:
     from __main__ import bot
+    import io
 
-    return bot.owner
+    def _get_line(l):
+        if l[0] == " " and l[1] != " ":
+            return l[1:]
+        return l
+
+    def _print(*args, **kwargs):
+        with io.StringIO() as output:
+            print(*args, file=output, end="", **kwargs)
+            return output.getvalue()
+
+    gs = globals() | {"print": _print}
+    ls = {}
+
+    lines = code.split(";")
+    for line in lines[:-1]:
+        exec(_get_line(line), gs, ls)
+    try:
+        return repr(eval(_get_line(lines[-1]), gs, ls))
+    except:
+        exec(_get_line(lines[-1]), gs, ls)
 
 
 async def main():
     evaluator = await connect_to_nqn()
-    owner = await evaluator.evaluate(_get_bot_owner)
-    print({slot: getattr(owner, slot) for slot in owner.__slots__})
+    print(await evaluator.evaluate(_eval, sys.argv[1]))
 
 
 if __name__ == "__main__":
